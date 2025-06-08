adbea61b3a46b6447e4e3f969885961c0d346b99
abdellah ait bachikh <abdo.jhon2023@gmail.com>
2025-06-08 14:43:16 +0100

    days->rowsperpage select

--- src/renderer/src/components/day/DaysList.tsx
+++ src/renderer/src/components/day/DaysList.tsx
@@ -1,4 +1,4 @@
-import { DatePicker, Pagination, Spinner } from '@heroui/react'
+import { DatePicker, Pagination, Select, SelectItem, Spinner } from '@heroui/react'
 import { getFullDate, getFullTime } from '@renderer/lib/utils'
 import Quantity from '../Quantity'
 import Amounth from '../Amounth'
@@ -6,13 +6,14 @@ import { useEffect, useState } from 'react'
 
 const DaysList = () => {
   const [page, setPage] = useState<number>(1)
+  const [rowsPerPage, setRowsPerPage] = useState<string>('all')
   const [filterDate, setFilterDate] = useState<Date | undefined>(undefined)
   const [loading, setLoading] = useState(false)
+
   useEffect(() => {
     console.log({ page, filterDate })
-
-    setLoading(!loading)
   }, [page, filterDate])
+  console.log(rowsPerPage)
   return (
     <div className="bg-white dark:bg-gray-950 p-3 rounded-xl mt-3 w-full">
       <div className="flex items-center justify-between gap-3">
@@ -50,8 +51,23 @@ const DaysList = () => {
                   <th className="font-semibold text-md text-center px-4 py-3 text-nowrap">
                     Montant
                   </th>
-                  <th className="font-semibold text-md text-center px-4 py-3 text-nowrap w-full">
-                    Action
+                  <th className="font-semibold text-md  px-4 py-3 text-nowrap w-full text-end">
+                    <Select
+                      aria-hidden="false"
+                      label="Line"
+                      size="sm"
+                      fullWidth={false}
+                      defaultSelectedKeys={[rowsPerPage]}
+                      onChange={(e) => setRowsPerPage(e.target.value)}
+                      value={rowsPerPage}
+                      variant="faded"
+                    >
+                      {[5, 10, 20, 'all'].map((item) => (
+                        <SelectItem key={item + ''} textValue={item + ''}>
+                          {item == 'all' ? 'Tous' : item}
+                        </SelectItem>
+                      ))}
+                    </Select>
                   </th>
                 </tr>
               </thead>
@@ -293,7 +309,6 @@ const DaysList = () => {
             onChange={setPage}
             total={10}
           />
-          
         </div>
       )}
     </div>
