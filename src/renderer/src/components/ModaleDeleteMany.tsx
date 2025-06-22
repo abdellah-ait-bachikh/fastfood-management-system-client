8942070124195fd0de05ad3c9c44671a24c6432c
abdellah ait bachikh <abdo.jhon2023@gmail.com>
2025-06-22 12:27:31 +0100

    remove commnts

--- src/renderer/src/components/ModaleDeleteMany.tsx
+++ src/renderer/src/components/ModaleDeleteMany.tsx
@@ -23,7 +23,7 @@ const ModaleDeleteMany = ({
   const [isOpen, setIsOpen] = useState<boolean>(false)
   const [loading, setLoading] = useState(false)
   const handleDelete = () => {
-    // Logique de suppression ici
+  
     // dispatch(
     //   deleteDay(id, setLoading, { dateFilter, page, rowsPerPage }, () => setSelectedId(null), {
     //     doCbIsFaled: true
@@ -48,7 +48,9 @@ const ModaleDeleteMany = ({
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
           <div className="bg-white dark:bg-gray-900 p-6 rounded-xl shadow-lg w-[90%] max-w-md text-center">
             <h2 className="text-xl font-semibold mb-4">Confirmation de suppression</h2>
-            <p className="mb-6">Êtes-vous sûr de vouloir supprimer l’élément #{ids.map(e=>e)} ?</p>
+            <p className="mb-6">
+              Êtes-vous sûr de vouloir supprimer l’élément #{ids.map((e) => e)} ?
+            </p>
             <div className="flex justify-center gap-4">
               <Button color="danger" onClick={handleDelete} isLoading={isOpen && loading}>
                 Oui, supprimer
